444b14284f0fa82f2821d415c44c33739903d33a
Colins Wanjao <wanjao.colins@gmail.com>
2024-08-22 23:11:11 +0300

    switched venv

--- FluxGenAIModel.py
+++ FluxGenAIModel.py
@@ -1,16 +1,24 @@
 import torch
 from diffusers import FluxPipeline
 
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
-pipe.enable_model_cpu_offload() # Remove this if you have enough GPU power
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
 
 prompt = "A cat holding a sign that says hello world"
 image = pipe(
     prompt,
-    guidance_scale=0.0,
-    num_inference_steps=4,
-    max_sequence_length=256,
+    height=1024,
+    width=1024,
+    guidance_scale=3.5,
+    num_inference_steps=50,
+    max_sequence_length=512,
     generator=torch.Generator("cpu").manual_seed(0)
 ).images[0]
-image.save("flux-schnell.png")
+image.save("flux-dev.png")
 
+
+ 
+
+
+
+# realistic image of an African leader at a ceremonial event launching a new road project. He is dressed in a traditional Kaunda suit. The setting is outdoors in a rural part of Kenya, with construction equipment and the newly laid road visible in the background. The leader is smiling and holding a pair of scissors, about to cut a ribbon"con
