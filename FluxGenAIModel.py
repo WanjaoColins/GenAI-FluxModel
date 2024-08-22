983bc5eb748e398dfbbe5c5d3a81551e43ff2bfd
Colins Wanjao <wanjao.colins@gmail.com>
2024-08-22 23:12:41 +0300

    modified prompt

--- FluxGenAIModel.py
+++ FluxGenAIModel.py
@@ -4,7 +4,7 @@ from diffusers import FluxPipeline
 pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
 pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
 
-prompt = "A cat holding a sign that says hello world"
+prompt = "realistic image of an African leader at a ceremonial event launching a new road project. He is dressed in a traditional Kaunda suit. The setting is outdoors in a rural part of Kenya, with construction equipment and the newly laid road visible in the background. The leader is smiling and holding a pair of scissors, about to cut a ribbon"
 image = pipe(
     prompt,
     height=1024,
@@ -15,10 +15,3 @@ image = pipe(
     generator=torch.Generator("cpu").manual_seed(0)
 ).images[0]
 image.save("flux-dev.png")
-
-
- 
-
-
-
-# realistic image of an African leader at a ceremonial event launching a new road project. He is dressed in a traditional Kaunda suit. The setting is outdoors in a rural part of Kenya, with construction equipment and the newly laid road visible in the background. The leader is smiling and holding a pair of scissors, about to cut a ribbon"con
